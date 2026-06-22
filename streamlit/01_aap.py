cc3445ce584229748d858d670b3c9d683da9a793
MD FIRDOSH ALAM <mdfirdoshalam851202@gmail.com>
2026-06-23 01:29:26 +0530

    Delete streamlit directory

--- streamlit/01_aap.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-# adding titile of your aap
-st.title("hello programer")
-st.subheader("welcome to my chnnel")
-st.write("chose a programming languge ")
-st.text("welcome to first streamit.app")
-var=st.selectbox("my fav team in ipl:",["kkr","mi","cks","rcb"])
-st.write(f"my fev team is {var}")
-st.success(f"you choise the team {var}")
